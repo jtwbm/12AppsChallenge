b49a985044d631fa48360aff2c56e0e754486d3f
Lyubov Bormotova <l.bormotova@leadgid.ru>
2020-01-23 11:32:06 +0300

    add some js

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,8 @@
 import './styles/app.scss';
+import $ from 'jquery';
 
+$('.next').on('click', function() {
+	$('html, body').stop().animate({
+		scrollTop: $('#about').offset().top - 50,
+	}, 500);
+});
